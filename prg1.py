1996a72b09527e994a5d1fd9dc81abfafb12977c
chitra728 <msc.saichitra@gmail.com>
2025-06-19 10:27:47 +0530

    Update prg1.py

--- prg1.py
+++ prg1.py
@@ -1,6 +1,4 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> a = 7
+ a = 7
 b = 2
 
 # addition
